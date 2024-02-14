76360929edb323ff37ad5bff2946d6a4c5efac4c
hoangSnapeV <90324439+hoangSnapeV@users.noreply.github.com>
2024-02-14 17:58:38 +0700

    Clear everything before studying on TET 2024

--- Knowledge/ComputerVision/downloadImage.py
+++ Knowledge/ComputerVision/downloadImage.py
@@ -2,6 +2,7 @@ import requests
 from PIL import Image
 from io import BytesIO
 import os
+import matplotlib.pyplot as plt
 
 def savePicture(urls):
     for url in urls:
@@ -9,3 +10,13 @@ def savePicture(urls):
         img = Image.open(BytesIO(response.content))
         filename = os.path.basename(url)
         img.save(f"image/{filename}")
+
+def plot_image(image_1, image_2,title_1="Orignal",title_2="New Image"):
+    plt.figure(figsize=(10,10))
+    plt.subplot(1, 2, 1)
+    plt.imshow(image_1)
+    plt.title(title_1)
+    plt.subplot(1, 2, 2)
+    plt.imshow(image_2)
+    plt.title(title_2)
+    plt.show()
